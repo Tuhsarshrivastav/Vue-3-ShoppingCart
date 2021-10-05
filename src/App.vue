63228c2b553fcdb610800a0bc7deb78d8a25cb93
Tusharshrivastav <ts3657@gmail.com>
2021-10-05 15:23:54 +0530

    added cart

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,14 @@
   <router-view />
 </template>
 
+<script>
+  export default {
+    mounted() {
+      this.$store.commit('updateCartFromLocalStorage')
+    }
+  }
+</script>
+
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    cart hai bhai yah
+    <h1>Your cart</h1>
+    <CartItemCard v-for="item in items" :key="item.id" :item="item" />
+    <CartSummaryPaymentCard />
   </div>
 </template>
 
 <script>
-export default {};
+import CartItemCard from "../components/cart/CartItemCard.vue";
+import CartSummaryPaymentCard from "../components/cart/CartSummaryPaymentCard.vue";
+export default {
+  components: {
+    CartItemCard,
+    CartSummaryPaymentCard,
+  },
+  computed: {
+    items() {
+      return this.$store.getters.cartItems;
+    },
+  },
+};
 </script>
-
-<style lang="scss" scoped></style>
