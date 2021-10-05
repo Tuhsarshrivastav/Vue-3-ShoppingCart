f83e7dc3e719939070e4ce2f14182e0c24010cb9
Tusharshrivastav <ts3657@gmail.com>
2021-10-05 13:02:38 +0530

    added cart

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@
         v-for="product in items"
         :key="product.id"
         :product="product"
+        v-on:view-product="viewProduct($event)"
       />
     </div>
   </div>
@@ -13,17 +14,23 @@
 <script>
 import items from "../data/items";
 import ProductSummaryCard from "../components/products/ProductSummaryCard.vue";
-import ProductDescriptionDrawer from "../components/products/ProductDescriptionDrawer.vue";
+// import ProductDescriptionDrawer from "../components/products/ProductDescriptionDrawer.vue";
 export default {
   name: "Home",
   components: {
     ProductSummaryCard,
-    ProductDescriptionDrawer,
+    // ProductDescriptionDrawer,
   },
   data() {
     return {
       items,
+      product: null,
     };
+  },
+  methods: {
+    viewProduct(product) {
+      this.product = product;
+    },
   },
 };
 </script>
