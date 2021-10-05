309e3d6ee9dd1c5d82abd3b413795c8e961267d6
Tusharshrivastav <ts3657@gmail.com>
2021-10-05 15:12:37 +0530

    added add to cart inc remove

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,49 @@
 <template>
   <div class="home">
+    <h1>Browse Our Selection</h1>
+    <ProductDescriptionDrawer
+      :product="product"
+      :active="active"
+      v-on:closeProductDrawer="closeProductDrawer()" />
     <div class="product-cards-container">
       <ProductSummaryCard
-        v-for="product in items"
-        :key="product.id"
-        :product="product"
-        v-on:view-product="viewProduct($event)"
-      />
-    </div>
+        v-for="(item, i) in items"
+        :key="i"
+        :product="item"
+        v-on:view-product="viewProduct($event)" />
+    </div>    
   </div>
 </template>
 
 <script>
-import items from "../data/items";
-import ProductSummaryCard from "../components/products/ProductSummaryCard.vue";
-// import ProductDescriptionDrawer from "../components/products/ProductDescriptionDrawer.vue";
+// @ is an alias to /src
+import items from '../data/items.js';
+import ProductSummaryCard from '../components/products/ProductSummaryCard.vue'
+import ProductDescriptionDrawer from '../components/products/ProductDescriptionDrawer.vue'
 export default {
   name: "Home",
-  components: {
-    ProductSummaryCard,
-    // ProductDescriptionDrawer,
-  },
+  components: {ProductSummaryCard,ProductDescriptionDrawer},
   data() {
     return {
-      items,
+      items: items,
       product: null,
-    };
+      active: {
+        product_drawer: false
+      }
+    }
   },
   methods: {
     viewProduct(product) {
-      this.product = product;
+      this.product = product
+      this.active.product_drawer = true
     },
-  },
+    closeProductDrawer() {
+      this.active.product_drawer = false
+    }
+  }
 };
 </script>
+
 <style lang="scss">
 .product-cards-container {
   display: flex;
